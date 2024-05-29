b2b4523eb38a9d60d800c174a1bd3c0a0b314a57
Caroalv <alvaradocarolinad2002@gmail.com>
2024-05-29 17:31:55 -0600

    cambios en la apk

--- src/app/shared/components/add-update-amarylidaceas/add-update-amarylidaceas.component.ts
+++ src/app/shared/components/add-update-amarylidaceas/add-update-amarylidaceas.component.ts
@@ -1,136 +1,117 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AmarylidaceasM } from 'src/app/models/amarylidaceas.model';
-import { User } from 'src/app/models/user.model';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { UtilsService } from 'src/app/services/utils.service';
+// Importaciones necesarias para el funcionamiento del componente
+import { Component, inject, Input, OnInit } from '@angular/core'; // Importa decoradores y funciones esenciales de Angular
+import { FormControl, FormGroup, Validators } from '@angular/forms'; // Importa clases para la creación de formularios reactivos y validadores
+import { AmarylidaceasM } from 'src/app/models/amarylidaceas.model'; // Importa el modelo de datos para Amarylidaceas
+import { FirebaseService } from 'src/app/services/firebase.service'; // Importa el servicio para interacciones con Firebase
+import { UtilsService } from 'src/app/services/utils.service'; // Importa un servicio de utilidades para manejar notificaciones y carga
 
+// Decorador que define el componente de Angular
 @Component({
-  selector: 'app-add-update-amarylidaceas',
-  templateUrl: './add-update-amarylidaceas.component.html',
-  styleUrls: ['./add-update-amarylidaceas.component.scss'],
+  selector: 'app-add-update-amarylidaceas', // Define el nombre del componente en el HTML
+  templateUrl: './add-update-amarylidaceas.component.html', // Ruta de la plantilla HTML del componente
+  styleUrls: ['./add-update-amarylidaceas.component.scss'], // Ruta de los estilos CSS del componente
 })
-export class AddUpdateAmarylidaceasComponent  implements OnInit {
-  
-  @Input() cultivos: AmarylidaceasM;
+export class AddUpdateAmarylidaceasComponent implements OnInit {
 
+  // Decorador @Input para recibir datos desde un componente padre
+  @Input() cultivos: AmarylidaceasM; // Propiedad de entrada que contiene datos de cultivos
+
+  // Definición del formulario reactivo con sus controles y validaciones
   form = new FormGroup({
-    aid: new FormControl(''),
-    nombre: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    imagen: new FormControl('', [Validators.required]),
-    descripcion: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    tiempoCosecha: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    fertilizacion: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    MarcoCultivo: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    Riego: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    cultivosCicloCorto: new FormControl('', [Validators.required, Validators.minLength(4)]),
-
+    aid: new FormControl(''), // Control para el ID del cultivo, sin validaciones
+    nombre: new FormControl('', [Validators.required, Validators.minLength(2)]), // Control para el nombre, requerido y con longitud mínima de 2 caracteres
+    imagen: new FormControl('', [Validators.required]), // Control para la URL de la imagen, requerido
+    descripcion: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para la descripción, requerido y con longitud mínima de 4 caracteres
+    tiempoCosecha: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para el tiempo de cosecha, requerido y con longitud mínima de 4 caracteres
+    fertilizacion: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para la fertilización, requerido y con longitud mínima de 4 caracteres
+    MarcoCultivo: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para el marco de cultivo, requerido y con longitud mínima de 4 caracteres
+    Riego: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para el riego, requerido y con longitud mínima de 4 caracteres
+    cultivosCicloCorto: new FormControl('', [Validators.required, Validators.minLength(4)]), // Control para cultivos de ciclo corto, requerido y con longitud mínima de 4 caracteres
   });
 
-  firebaseSvc= inject(FirebaseService);
-  utilsSvc= inject(UtilsService)
+  // Inyección de dependencias utilizando la API de inyección de Angular
+  firebaseSvc = inject(FirebaseService); // Servicio de Firebase para operaciones CRUD
+  utilsSvc = inject(UtilsService); // Servicio de utilidades para manejar mensajes y modales
 
-
+  // Método del ciclo de vida OnInit de Angular, se ejecuta al inicializar el componente
   ngOnInit() {
-    if (this.cultivos) this.form.setValue(this.cultivos);
-
+    if (this.cultivos) { // Verifica si se han pasado datos de cultivos
+      this.form.setValue(this.cultivos); // Si hay datos, establece los valores del formulario con esos datos
+    }
   }
 
-  submit(){
-    if (this.form.valid) {
-      if(this.cultivos) this.updateCultivos();
-      else this.createCultivos();
-    }   
-  }
- 
-  
-  ////////// CREAR CULTIVOS /////////////
-  async createCultivos(){
-
-
-      let path = `/Amarylidáceas`
-      const loading = await this.utilsSvc.loading();
-      await loading.present();
-      
-      delete this.form.value.aid;
-
-      this.firebaseSvc.addDocument(path, this.form.value).then(async res =>{
-        
-        this.utilsSvc.dismissModal({ successs: true});
-
-        this.utilsSvc.presentToast({
-          message: 'Cultivo creado exitosamente',
-          duration: 1500,
-          color: 'success',
-          position: 'middle',
-          icon: 'checkmark-circle-outline'
-        })     
-
-      }).catch(error=>{
-        console.log(error);
-
-        this.utilsSvc.presentToast({
-          message: error.message,
-          duration: 2500,
-          color: 'primary',
-          position: 'middle',
-          icon: 'alert-circle-outline'
-        })
-
-
-      }).finally(()=> {
-        loading.dismiss();
-      })
-
-
-
+  // Método que se ejecuta al enviar el formulario
+  submit() {
+    if (this.form.valid) { // Verifica si el formulario es válido
+      if (this.cultivos) { // Si hay datos de cultivos, es una actualización
+        this.updateCultivos(); // Llama al método para actualizar cultivos
+      } else { // Si no hay datos de cultivos, es una creación
+        this.createCultivos(); // Llama al método para crear cultivos
+      }
+    }
   }
 
+  // Método asíncrono para crear nuevos cultivos en Firebase
+  async createCultivos() {
+    let path = '/Amarylidáceas'; // Define la ruta de la colección en Firebase
+    const loading = await this.utilsSvc.loading(); // Muestra un indicador de carga
+    await loading.present(); // Espera a que el indicador de carga se presente
 
-//==================== Actualizar Producto ======================
-async updateCultivos() {
+    delete this.form.value.aid; // Elimina el ID del cultivo del formulario, ya que no se necesita para crear
 
+    // Agrega un nuevo documento en Firebase y maneja la respuesta
+    this.firebaseSvc.addDocument(path, this.form.value).then(async res => {
+      this.utilsSvc.dismissModal({ successs: true }); // Cierra el modal y envía una señal de éxito
+      this.utilsSvc.presentToast({
+        message: 'Cultivo creado exitosamente', // Mensaje de éxito
+        duration: 1500, // Duración del mensaje
+        color: 'success', // Color del mensaje
+        position: 'middle', // Posición del mensaje en la pantalla
+        icon: 'checkmark-circle-outline' // Icono del mensaje
+      });
+    }).catch(error => {
+      console.error(error); // Imprime el error en la consola
+      this.utilsSvc.presentToast({
+        message: error.message, // Mensaje de error
+        duration: 2500, // Duración del mensaje
+        color: 'primary', // Color del mensaje
+        position: 'middle', // Posición del mensaje en la pantalla
+        icon: 'alert-circle-outline' // Icono del mensaje
+      });
+    }).finally(() => {
+      loading.dismiss(); // Oculta el indicador de carga
+    });
+  }
 
-  let path =`/Amarylidáceas/${this.cultivos.aid}`
+  // Método asíncrono para actualizar cultivos existentes en Firebase
+  async updateCultivos() {
+    let path = `/Amarylidáceas/${this.cultivos.aid}`; // Define la ruta del documento en Firebase usando el ID del cultivo
+    const loading = await this.utilsSvc.loading(); // Muestra un indicador de carga
+    await loading.present(); // Espera a que el indicador de carga se presente
 
-  const loading = await this.utilsSvc.loading();
-  await loading.present();
+    delete this.form.value.aid; // Elimina el ID del cultivo del formulario, ya que no se necesita para actualizar
 
-  delete this.form.value.aid;
-
-
-  this.firebaseSvc.updateDocument(path, this.form.value).then(async res => {
-    
-    this.utilsSvc.dismissModal({ successs: true});
-
-    this.utilsSvc.presentToast({
-      message: 'Producto actualizado exitosamente',
-      duration: 1500,
-      color: 'success',
-      position: 'middle',
-      icon: 'checkmark-circle-outline'
-    })
-
-
-  }).catch(error=>{
-    console.log("error");
-
-    this.utilsSvc.presentToast({
-      message: error.message,
-      duration: 2500,
-      color: 'primary',
-      position: 'middle',
-      icon: 'alert-circle-outline'
-    })
-
-  }).finally(()=> {
-    loading.dismiss();
-  })
-
+    // Actualiza el documento en Firebase y maneja la respuesta
+    this.firebaseSvc.updateDocument(path, this.form.value).then(async res => {
+      this.utilsSvc.dismissModal({ successs: true }); // Cierra el modal y envía una señal de éxito
+      this.utilsSvc.presentToast({
+        message: 'Producto actualizado exitosamente', // Mensaje de éxito
+        duration: 1500, // Duración del mensaje
+        color: 'success', // Color del mensaje
+        position: 'middle', // Posición del mensaje en la pantalla
+        icon: 'checkmark-circle-outline' // Icono del mensaje
+      });
+    }).catch(error => {
+      console.error(error); // Imprime el error en la consola
+      this.utilsSvc.presentToast({
+        message: error.message, // Mensaje de error
+        duration: 2500, // Duración del mensaje
+        color: 'primary', // Color del mensaje
+        position: 'middle', // Posición del mensaje en la pantalla
+        icon: 'alert-circle-outline' // Icono del mensaje
+      });
+    }).finally(() => {
+      loading.dismiss(); // Oculta el indicador de carga
+    });
+  }
 }
-
-
-
-
-}
-
